b4cda06d4ead73d2e640518c105340582bbaaa29
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-02-15 15:56:49 +0100

    updated Main (draft) and .gitignore (changed for C# -> previously was for Java)

--- BattleshipWU/Program.cs
+++ BattleshipWU/Program.cs
@@ -6,7 +6,7 @@ namespace BattleshipWU {
         static void Main(string[] args) {
             Console.WriteLine("That is battleship game!");
             Ocean ocean1 = new Ocean(10);
-            Ship ship1 = new Ship(3, Ship.Layout.HORIZONTAL);
+            Ship ship1 = new Ship(Ship.ShipType.S, Ship.Layout.HORIZONTAL);
             ocean1.placeShipAtTheOcean(ship1, 2, 2, new Square(Square.SquareType.SHIP), new Square(Square.SquareType.OCEAN));
             ocean1.displayOcean();
         }
