1ffdfa62a967a29e7869e025ec8bc6a41bd0cb9d
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-02-16 16:35:01 +0100

    getShipsPositions method in Player class: added user incorrect input handling

--- BattleshipWU/Player.cs
+++ BattleshipWU/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BattleshipWU {
     class Player {
@@ -19,14 +20,46 @@ namespace BattleshipWU {
 
             foreach (string type in shipTypesNames) {
                 Console.WriteLine("\nPlease place " + type + " squares) on your ocean:");
-                Console.WriteLine("Position:");
-                string position = Console.ReadLine().ToUpper();
-                Console.WriteLine("Layout (horizontal/vertical):");
-                string layout = Console.ReadLine().ToUpper();
+                
+                int positionX = -1;
+                int positionY = -1;
+                while (positionX == -1 || positionY == -1) {
+                    Console.WriteLine("Position:");
+                    string position = Console.ReadLine().ToUpper();
 
-                char[] shipPosition = position.ToCharArray();
-                int positionX = shipPosition[1] - 49;
-                int positionY = (int)shipPosition[0] - 65;
+                    if (isLetter(position[0].ToString())) {
+                        positionY = (int)position[0] - 65;
+                        if (positionY >= ocean.Dimension) {
+                            positionY = -1;
+                            Console.WriteLine("Column index exceeded board dimension");
+                        }
+                        if (isNumeric(position.Substring(1))) {
+                            positionX = Int32.Parse(position.Substring(1)) - 1;
+                            if (positionX >= ocean.Dimension) {
+                                positionX = -1;
+                                Console.WriteLine("Row index exceeded board dimension");
+                            }
+                        } else {
+                            Console.WriteLine("Invalid row number");
+                        }
+                    }
+                    else {
+                        Console.WriteLine("First input character must be a letter indcating column");
+                    }
+                }
+
+                string layout = null;
+                while (layout == null) {
+                    Console.WriteLine("Layout (h - horizontal/v - vertical):");
+                    string layoutInput = Console.ReadLine().ToLower();
+                    if (layoutInput == "h") {
+                        layout = "HORIZONTAL";
+                    } else if (layoutInput == "v") {
+                        layout = "VERTICAL";
+                    } else {
+                        Console.WriteLine("Wrong input");
+                    }
+                }
 
                 int index = shipTypesNames.IndexOf(type);
                 Ship ship = new Ship(shipTypes[index], layout);
@@ -48,6 +81,16 @@ namespace BattleshipWU {
             }
             */
             return myOcean;
+        }
+
+        public static bool isNumeric(string strToCheck) {
+            Regex rg = new Regex(@"^[0-9\s,]*$");
+            return rg.IsMatch(strToCheck);
+        }
+
+        public static bool isLetter(string strToCheck) {
+            Regex rg = new Regex(@"^[a-zA-Z\s,]*$");
+            return rg.IsMatch(strToCheck);
         }
     }
 }
